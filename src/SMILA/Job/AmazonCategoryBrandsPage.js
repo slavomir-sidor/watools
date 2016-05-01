175424ab946a14a3353dc0dc383fe8d518f5a812
Slavomir Kuzma <slavomir.kuzma@gmail.com>
2016-05-01 12:48:05 +0200

    Removed slash for brand url.

--- src/SMILA/Job/AmazonCategoryBrandsPage.js
+++ src/SMILA/Job/AmazonCategoryBrandsPage.js
@@ -15,7 +15,7 @@ function AmazonCategoryBrandsPage(amazonCode, letter, categoryId)
 	this.categoryId = categoryId;
 	this.url = "http://www.amazon.com/gp/search/other?page=1&pickerToList=brandtextbin&rh=n:" + amazonCode
 			+ "&indexField=" + letter;
-}
+};
 
 AmazonCategoryBrandsPage.prototype = new Webpage();
 AmazonCategoryBrandsPage.prototype.constructor = AmazonCategoryBrandsPage;
@@ -24,22 +24,21 @@ AmazonCategoryBrandsPage.prototype.processPage = function(callback)
 	Webpage.prototype.processPage.call(this, callback);
 
 	var self = this;
-	
 	var brands = this.getPage().evaluate(function()
 	{
 		var brandsElements = $(".s-see-all-indexbar-column a");
 		var brands = new Array();
-		
+
 		$.each(brandsElements, function(index, brandElement)
 		{
 			var element = $(brandElement);
 			var text = element.text();
-			var url = 'http://www.amazon.com/' + element.attr('href');
-			var productCountRegExp = new RegExp("[0-9]+","g");
+			var url = 'http://www.amazon.com' + element.attr('href');
+			var productCountRegExp = new RegExp("[0-9]+", "g");
 			var productInfoRegExp = new RegExp("\\ \\([0-9]+\\)", "g");
 			var productCountMatches = productCountRegExp.exec(text);
-			var productCount=productCountMatches[0];
-			var name=text.replace(productInfoRegExp,"");
+			var productCount = productCountMatches[0];
+			var name = text.replace(productInfoRegExp, "");
 
 			var brand =
 			{
@@ -48,17 +47,18 @@ AmazonCategoryBrandsPage.prototype.processPage = function(callback)
 				Url : url,
 				ProductCount : productCount
 			};
+			console.log(brand.ProductCount);
 
 			brands.push(brand);
 		});
-		
-		var postGoogleLogo=function(brand)
+
+		var postGoogleLogo = function(brand)
 		{
-			//AmazonBrandGoogleLogo
+			// AmazonBrandGoogleLogo
 		};
 
 		console.log('Found ' + brands.length + ' brands.');
-		
+
 		var i = 0;
 		var postBrand = function()
 		{
